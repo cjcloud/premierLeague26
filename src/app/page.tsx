e76d296572eeec8db7841555a225c3a76cd6e8b1
cjcloud <cjcloudsolutions@gmail.com>
2025-08-10 00:50:41 +0100

    feat: create landing page with leaderboard access and points system info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Button size="default" variant="outline">Make Predictions</Button>
           </Link>
         </div>
-      <div className="mx-auto bg-slate-400">
+      <div className="mx-auto px-4 py-4 rounded-lg bg-slate-400">
         <p className="sm:text-[6px] md:text-lg font-bold mt-4">Points System</p>
         <p className="sm:text-[6px] md:text-lg">Correct prediction = 2 points</p> 
         <p className="sm:text-[6px] md:text-lg">Near miss = 1 point</p>
