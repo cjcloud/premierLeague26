5efda9fdead2f9f4476af9c8f92a2b9f56417cb2
cjcloud <cjcloudsolutions@gmail.com>
2025-08-10 01:21:43 +0100

    feat: create homepage with points system and navigation buttons for Premier League predictions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <main className="container max-w-2xl mx-auto px-4 py-8" style={{ marginTop: '30px' }}>
       <div className="flex flex-col items-center justify-center gap-6">
-        <h2 className="sm:text-md md:text-xl lg:text-2xl font-bold text-center">Welcome to Premier League Predictions</h2>
+        <h2 className="sm:text-md md:text-xl lg:text-2xl font-bold text-center">Welcome to</h2>
+        <h2 className="sm:text-md md:text-xl lg:text-2xl font-bold text-center">Premier League</h2>
+        <h2 className="sm:text-md md:text-xl lg:text-2xl font-bold text-center">Predictions</h2>
         <p className="text-muted-foreground sm:text-xs md:text-md text-center max-w-md">
           Make your selections before Aug 15th at 7pm
         </p>
@@ -17,8 +19,8 @@ export default function Home() {
             <Button size="default" variant="outline">Make Predictions</Button>
           </Link>
         </div>
-      <div className="mx-auto px-4 py-4 rounded-lg bg-slate-200">
-        <p className="sm:text-[6px] md:text-lg font-bold mt-4">Points System</p>
+      <div className="mx-auto px-4 rounded-lg bg-slate-200">
+        <p className="sm:text-[6px] md:text-lg font-bold">Points System</p>
         <p className="sm:text-[6px] md:text-lg">Correct prediction = 2 points</p> 
         <p className="sm:text-[6px] md:text-lg">Near miss = 1 point</p>
 
