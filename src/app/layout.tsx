a2cb3808b67c05c92960b66bd6fe1674dba7b4f1
cjcloud <cjcloudsolutions@gmail.com>
2025-08-10 17:23:24 +0100

    feat: add favicon assets and configure root layout with metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Premier League Predictions",
   description: "Premier League Predictions",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon.ico' }
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png' }
+    ],
+    other: [
+      { url: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+    ]
+  },
 };
 
 export const viewport: Viewport = {
